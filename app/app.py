e10e1433f30de9cecee857e3fcf2f72bb92eb023
Jack <112922957+Hotpocket106@users.noreply.github.com>
2023-02-22 23:31:35 -0600

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask,request, render_template
+import numpy as np
+import tensorflow as tf
+
+app = Flask(__name__)
+
+model =tf.keras.models.load_model('my_model')
+
+
+@app.route('/')
+def hello_world():
+    return render_template("inputs.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    final = np.reshape(final, (1, -1))
+    
+    print(int_features)
+    print(final)
+    prediction=model.predict(final)
+    # output = prediction[0]
+    output=prediction[0]
+
+    if output > 0.5:
+        return render_template('inputs.html',pred='Your health is in Danger.\nProbability of Diabetes score is {}'.format(output*100))
+    else:
+        return render_template('inputs.html',pred='You are NOT likely to have Diabetes.\n Probability of Diabetes score is {}'.format(output*100))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ app/train_app.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from pathlib import Path
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+
+file_path = ('diabetes.csv')
+df = pd.read_csv(file_path)
+
+catcol = ['HighBP', 'CholCheck','BMI',
+       'Smoker', 'Stroke', 'PhysActivity','NoDocbcCost', 'Sex','Age', 'Education']
+
+new_df = df[catcol]
+new_df.head()
+
+y = df['Diabetes_binary'].values
+X = new_df.values
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=78)
+
+scaler = StandardScaler()
+
+X_scaler = scaler.fit(X_train)
+
+X_train_scaled = X_scaler.transform(X_train)
+X_test_scaled = X_scaler.transform(X_test)
+
+number_input_features = len(X_train[0])
+hidden_nodes_layer1 = 80
+hidden_nodes_layer2 = 30
+hidden_nodes_layer3 = 30
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(units=hidden_nodes_layer1, activation='relu', input_shape=(10,)),
+    tf.keras.layers.Dense(units=hidden_nodes_layer2, activation='relu'),
+    tf.keras.layers.Dense(units=hidden_nodes_layer3, activation='relu'),
+    tf.keras.layers.Dense(units=1, activation="sigmoid")
+])
+
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+model.fit(X_train, y_train, epochs=10, batch_size=32)
+
+model.save('my_model')
